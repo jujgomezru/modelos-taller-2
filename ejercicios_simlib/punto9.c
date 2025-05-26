e15661e0dd7776ed4bb073c29488fb1bf58e2d6c
jujgomezru <jujgomezru@unal.edu.co>
2025-05-25 21:15:00 -0500

    Nuevo modelo simpy

--- ejercicios_simlib/punto9.c
+++ ejercicios_simlib/punto9.c
@@ -7,12 +7,14 @@
 #define LIST_SERVER1 2
 #define LIST_QUEUE2 3
 #define LIST_SERVER2 4
-#define SAMPST_DELAYS 1
+#define SAMPST_DELAYS_Q1 1    // Separate delay tracking for Queue 1
+#define SAMPST_DELAYS_Q2 2    // Separate delay tracking for Queue 2
+#define SAMPST_TOTAL_TIME 3   // For total system time
 #define STREAM_INTERARRIVAL 1
 #define STREAM_SERVICE 2
 
-int num_custs_delayed, total_simulation_time, num_completed_customers, 
-distribution_type,servers_level1, servers_level2;
+int num_custs_delayed, total_simulation_time, num_completed_customers, distribution_type;
+int servers_level1, servers_level2;  // Number of servers at each level
 float mean_interarrival, mean_service;
 FILE *infile, *outfile;
 
@@ -44,18 +46,22 @@ float get_service_time() {
 
 void init_model(void) {
     num_custs_delayed = 0;
+    num_completed_customers = 0;
     event_schedule(sim_time + get_interarrival_time(), EVENT_ARRIVAL);
 }
 
 void arrive(void) {
     event_schedule(sim_time + get_interarrival_time(), EVENT_ARRIVAL);
+
+    // Store arrival time in transfer[1] and queue1 entry time in transfer[2]
+    transfer[1] = sim_time;  // Arrival time
+    transfer[2] = sim_time;  // Initially same as arrival, may be updated if queued
     
-    if (list_size[LIST_SERVER1] >= servers_level1) {  // Check against number of servers
-        transfer[1] = sim_time;
+    if (list_size[LIST_SERVER1] >= servers_level1) {
         list_file(LAST, LIST_QUEUE1);
     }
     else {
-        sampst(0.0, SAMPST_DELAYS);
+        sampst(0.0, SAMPST_DELAYS_Q1);  // No delay in queue 1
         ++num_custs_delayed;
         list_file(FIRST, LIST_SERVER1);
         event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE1);
@@ -63,42 +69,64 @@ void arrive(void) {
 }
 
 void depart1(void) {
-    if (list_size[LIST_QUEUE1] == 0) {
-        list_remove(FIRST, LIST_SERVER1);
-    } else {
-        list_remove(FIRST, LIST_QUEUE1);
-        sampst(sim_time - transfer[1], SAMPST_DELAYS);
-        ++num_custs_delayed;
-        event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE1);
-    }
+    // Remove the customer who just finished service at server1
+    list_remove(FIRST, LIST_SERVER1);
 
-    if (list_size[LIST_SERVER2] >= servers_level2) {  // Check against number of servers
-        transfer[1] = sim_time;
+    // Process this customer's movement to level2
+    transfer[3] = sim_time;  // Time entering queue2/service2
+    
+    if (list_size[LIST_SERVER2] >= servers_level2) {
+        transfer[4] = sim_time;  // Record queue2 entry time
         list_file(LAST, LIST_QUEUE2);
     }
     else {
+        sampst(0.0, SAMPST_DELAYS_Q2);  // No delay in queue2
+        list_file(FIRST, LIST_SERVER2);
+        event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE2);
+    }
+
+    // Now check if there are customers in queue1 to serve in server1
+    if (list_size[LIST_QUEUE1] > 0) {
+        list_remove(FIRST, LIST_QUEUE1);
+        sampst(sim_time - transfer[2], SAMPST_DELAYS_Q1);  // Queue1 delay
+        ++num_custs_delayed;
+        list_file(FIRST, LIST_SERVER1);
+        event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE1);
+    }
+}
+
+void depart2(void) {
+    // Remove the customer who just finished service at server2
+    list_remove(FIRST, LIST_SERVER2);
+
+    // Calculate and record total system time using original arrival time
+    float system_time = sim_time - transfer[1];
+    sampst(system_time, SAMPST_TOTAL_TIME);
+    ++num_completed_customers;
+
+    // Check if there are customers in queue2 to serve
+    if (list_size[LIST_QUEUE2] > 0) {
+        list_remove(FIRST, LIST_QUEUE2);
+        sampst(sim_time - transfer[4], SAMPST_DELAYS_Q2);  // Queue2 delay
         list_file(FIRST, LIST_SERVER2);
         event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE2);
     }
 }
 
-void depart2(void) {
-    if (list_size[LIST_QUEUE2] == 0) {
-        list_remove(FIRST, LIST_SERVER2);
-    } else {
-        list_remove(FIRST, LIST_QUEUE2);
-        event_schedule(sim_time + get_service_time(), EVENT_DEPARTURE2);
-    }
-
-    ++num_completed_customers;
-}
-
 void report(void) {
-    fprintf(outfile, "\nDelays in queue, in minutes:\n");
-    out_sampst(outfile, SAMPST_DELAYS, SAMPST_DELAYS);
-    fprintf(outfile, "\nQueue length and server utilization:\n");
+    fprintf(outfile, "\nDelays in Queue 1, in minutes:\n");
+    out_sampst(outfile, SAMPST_DELAYS_Q1, SAMPST_DELAYS_Q1);
+    
+    fprintf(outfile, "\nDelays in Queue 2, in minutes:\n");
+    out_sampst(outfile, SAMPST_DELAYS_Q2, SAMPST_DELAYS_Q2);
+    
+    fprintf(outfile, "\nTotal System Time (arrival to final departure), in minutes:\n");
+    out_sampst(outfile, SAMPST_TOTAL_TIME, SAMPST_TOTAL_TIME);
+    
+    fprintf(outfile, "\nQueue lengths and server utilization:\n");
     out_filest(outfile, LIST_QUEUE1, LIST_SERVER1);
     out_filest(outfile, LIST_QUEUE2, LIST_SERVER2);
+    
     fprintf(outfile, "\nTime simulation ended:%12.3f minutes\n", sim_time);
     fprintf(outfile, "Number of customers completed both services:%8d\n", num_completed_customers);
     fprintf(outfile, "Number of servers at level 1: %d\n", servers_level1);
@@ -110,7 +138,7 @@ int main() {
 
     infile = fopen("parameters_9.txt", "r");
     if (infile == NULL) {
-        printf("Error: cannot open input file parameters_9.txt\n");
+        printf("Error: cannot open input file mm9smlb.in\n");
         return 1;
     }
 
@@ -121,7 +149,7 @@ int main() {
            &servers_level1, &servers_level2);
 
     // Generate output filename
-    sprintf(outfilename, "report_9_%d.txt", distribution_type, servers_level1, servers_level2);
+    sprintf(outfilename, "report_9_%d.txt", distribution_type);
 
     outfile = fopen(outfilename, "w");
     if (outfile == NULL) {
@@ -154,7 +182,7 @@ int main() {
     fprintf(outfile, "Number of servers at level 1: %d\n", servers_level1);
     fprintf(outfile, "Number of servers at level 2: %d\n\n", servers_level2);
 
-    maxatr = 4;
+    maxatr = 10;
     maxlist = 26;
     init_simlib();
     init_model();
--- ejercicios_simpy/punto9.py
+++ ejercicios_simpy/punto9.py
@@ -1,227 +1,246 @@
 import simpy
 import random
 import statistics
+from collections import defaultdict
 
-def get_interarrival_time(distribution_type, mean, rng):
-    if distribution_type in [1, 3]:
-        return rng.expovariate(1.0 / mean)
-    elif distribution_type in [2, 4]:
-        return mean
-    else:
-        return rng.expovariate(1.0 / mean)
-
-def get_service_time(distribution_type, mean, rng):
-    if distribution_type in [1, 2]:
-        return rng.expovariate(1.0 / mean)
-    elif distribution_type in [3, 4]:
-        return mean
-    else:
-        return rng.expovariate(1.0 / mean)
-
-def enqueue(env, data):
-    now = env.now
-    if data['last_time'] < now:
-        delta = now - data['last_time']
-        data['area'] += data['current_length'] * delta
-        data['last_time'] = now
-    data['current_length'] += 1
-
-def dequeue(env, data):
-    now = env.now
-    if data['last_time'] < now:
-        delta = now - data['last_time']
-        data['area'] += data['current_length'] * delta
-        data['last_time'] = now
-    data['current_length'] -= 1
-
-def user_in(env, data):
-    now = env.now
-    if data['last_time'] < now:
-        delta = now - data['last_time']
-        data['area'] += data['current_count'] * delta
-        data['last_time'] = now
-    data['current_count'] += 1
-
-def user_out(env, data):
-    now = env.now
-    if data['last_time'] < now:
-        delta = now - data['last_time']
-        data['area'] += data['current_count'] * delta
-        data['last_time'] = now
-    data['current_count'] -= 1
-
-def finalize_data(data, total_time):
-    now = total_time
-    if data['last_time'] < now:
-        delta = now - data['last_time']
-        if 'current_length' in data:
-            data['area'] += data['current_length'] * delta
+class TwoLevelQueueSystem:
+    def __init__(self, env, mean_interarrival, mean_service, 
+                 servers_level1, servers_level2, distribution_type):
+        self.env = env
+        self.mean_interarrival = mean_interarrival
+        self.mean_service = mean_service
+        self.distribution_type = distribution_type
+        self.server1 = simpy.Resource(env, capacity=servers_level1)
+        self.server2 = simpy.Resource(env, capacity=servers_level2)
+        self.queue1_delays = []
+        self.queue2_delays = []
+        self.system_times = []
+        self.queue1_lengths = []
+        self.queue2_lengths = []
+        self.server1_busy = []
+        self.server2_busy = []
+        self.num_completed = 0
+        self.num_arrivals = 0
+        self.queue1_entry_times = {}
+        self.queue2_entry_times = {}
+        self.env.process(self.customer_generator())
+        self.env.process(self.monitor_queues())
+        self.env.process(self.monitor_servers())
+    
+    def get_interarrival_time(self):
+        if self.distribution_type in [1, 3]:
+            return random.expovariate(1.0 / self.mean_interarrival)
         else:
-            data['area'] += data['current_count'] * delta
-        data['last_time'] = now
-    return data['area'] / total_time if total_time > 0 else 0.0
-
-def customer_process(env, server1, s1_q_data, s1_u_data, s1_delays,
-                    server2, s2_q_data, s2_u_data, s2_delays,
-                    total_delays, completed,
-                    dist_type, mean_service, service_rng):
-    # Level 1 processing
-    arrival_time1 = env.now
-    enqueue(env, s1_q_data)
-    req = server1.request()
-    yield req
-    dequeue(env, s1_q_data)
-    user_in(env, s1_u_data)
-
-    delay1 = env.now - arrival_time1
-    s1_delays.append(delay1)
-
-    service_time = get_service_time(dist_type, mean_service, service_rng)
-    yield env.timeout(service_time)
-
-    user_out(env, s1_u_data)
-    server1.release(req)
-
-    # Level 2 processing
-    arrival_time2 = env.now
-    enqueue(env, s2_q_data)
-    req2 = server2.request()
-    yield req2
-    dequeue(env, s2_q_data)
-    user_in(env, s2_u_data)
-
-    delay2 = env.now - arrival_time2
-    s2_delays.append(delay2)
-
-    service_time2 = get_service_time(dist_type, mean_service, service_rng)
-    yield env.timeout(service_time2)
-
-    user_out(env, s2_u_data)
-    server2.release(req2)
-
-    # Record total delay
-    total_delays.append(delay1 + delay2)
-    completed.append(1)
-
-def arrival_process(env, server1, server2, dist_type, mean_iat,
-                   s1_q_data, s1_u_data, s1_delays,
-                   s2_q_data, s2_u_data, s2_delays,
-                   total_delays, completed,
-                   mean_service, iat_rng, service_rng):
-    while True:
-        iat = get_interarrival_time(dist_type, mean_iat, iat_rng)
-        yield env.timeout(iat)
-        env.process(customer_process(
-            env, server1, s1_q_data, s1_u_data, s1_delays,
-            server2, s2_q_data, s2_u_data, s2_delays,
-            total_delays, completed,
-            dist_type, mean_service, service_rng
-        ))
-
-def main():
-    # Read parameters
-    with open('parameters_9.txt', 'r') as f:
-        params = f.read().split()
-    mean_iat = float(params[0])
-    mean_service = float(params[1])
-    sim_time = int(float(params[2]))
-    dist_type = int(params[3])
-    servers_l1 = int(params[4])
-    servers_l2 = int(params[5])
-
-    # Setup environment and resources
-    env = simpy.Environment()
-    server1 = simpy.Resource(env, capacity=servers_l1)
-    server2 = simpy.Resource(env, capacity=servers_l2)
-
-    # Initialize data collectors
-    s1_q_data = {'last_time': 0, 'current_length': 0, 'area': 0.0}  # Queue length
-    s1_u_data = {'last_time': 0, 'current_count': 0, 'area': 0.0}   # Utilization
-    s2_q_data = {'last_time': 0, 'current_length': 0, 'area': 0.0}  # Queue length
-    s2_u_data = {'last_time': 0, 'current_count': 0, 'area': 0.0}   # Utilization
-    s1_delays = []  # Level 1 queue delays
-    s2_delays = []  # Level 2 queue delays
-    total_delays = []  # Sum of both queue delays
-    completed = []
-
-    # Setup random generators
-    iat_rng = random.Random()
-    service_rng = random.Random()
-
-    # Start processes
-    env.process(arrival_process(
-        env, server1, server2, dist_type, mean_iat,
-        s1_q_data, s1_u_data, s1_delays,
-        s2_q_data, s2_u_data, s2_delays,
-        total_delays, completed,
-        mean_service, iat_rng, service_rng
-    ))
-
-    # Run simulation
-    env.run(until=sim_time)
-
-    # Calculate statistics
-    avg_q1 = finalize_data(s1_q_data, sim_time)
-    util1 = finalize_data(s1_u_data, sim_time) / servers_l1
-    avg_q2 = finalize_data(s2_q_data, sim_time)
-    util2 = finalize_data(s2_u_data, sim_time) / servers_l2
-
-    # Write report
-    output_file = f'report_9_{dist_type}.txt'
-    with open(output_file, 'w') as f:
-        f.write(f"Two-Level Queueing System Report\n")
-        f.write(f"="*50 + "\n\n")
-        
-        # System Configuration
-        f.write(f"System Configuration:\n")
-        f.write(f"- Mean interarrival time: {mean_iat:.3f} minutes\n")
-        f.write(f"- Mean service time: {mean_service:.3f} minutes\n")
-        f.write(f"- Level 1 Servers: {servers_l1}\n")
-        f.write(f"- Level 2 Servers: {servers_l2}\n")
-        f.write(f"- Distribution type: {dist_type} (")
-        f.write({
-            1: "Exponential/Exponential",
-            2: "Constant/Exponential", 
-            3: "Exponential/Constant",
-            4: "Constant/Constant"
-        }.get(dist_type, "Exponential/Exponential") + ")\n\n")
-        
-        # Combined Delay Statistics
-        f.write(f"Combined Delay Statistics (Queue 1 + Queue 2):\n")
-        if total_delays:
-            f.write(f"- Average total delay: {statistics.mean(total_delays):.3f} minutes\n")
-            f.write(f"- Maximum total delay: {max(total_delays):.3f} minutes\n")
-            f.write(f"- Minimum total delay: {min(total_delays):.3f} minutes\n")
+            return self.mean_interarrival
+    
+    def get_service_time(self):
+        if self.distribution_type in [1, 2]:
+            return random.expovariate(1.0 / self.mean_service)
         else:
-            f.write("No customers completed both queues\n")
-        f.write("\n")
+            return self.mean_service
+    
+    def customer_generator(self):
+        while True:
+            # Schedule next arrival first (like in original arrive function)
+            yield self.env.timeout(self.get_interarrival_time())
+            
+            # Create new customer (equivalent to transfer[1] = sim_time in original)
+            arrival_time = self.env.now
+            self.num_arrivals += 1
+            customer_id = self.num_arrivals
+            
+            # Process customer through the system
+            self.env.process(self.customer_flow(customer_id, arrival_time))
+    
+    def customer_flow(self, customer_id, arrival_time):
+        """Handles the complete flow through both queues and servers"""
+        # Level 1 processing (equivalent to original arrive and depart1 logic)
         
-        # Queue 1 Statistics
-        f.write(f"Queue 1 Statistics:\n")
-        if s1_delays:
-            f.write(f"- Average delay: {statistics.mean(s1_delays):.3f} minutes\n")
-            f.write(f"- Maximum delay: {max(s1_delays):.3f} minutes\n")
-            f.write(f"- Minimum delay: {min(s1_delays):.3f} minutes\n")
-        else:
-            f.write("- No customers completed Queue 1\n")
-        f.write(f"- Average queue length: {avg_q1:.3f}\n")
-        f.write(f"- Server utilization: {util1:.3f}\n\n")
+        # Record queue1 entry time (transfer[2] in original)
+        queue1_entry = self.env.now
+        self.queue1_entry_times[customer_id] = queue1_entry
         
-        # Queue 2 Statistics
-        f.write(f"Queue 2 Statistics:\n")
-        if s2_delays:
-            f.write(f"- Average delay: {statistics.mean(s2_delays):.3f} minutes\n")
-            f.write(f"- Maximum delay: {max(s2_delays):.3f} minutes\n")
-            f.write(f"- Minimum delay: {min(s2_delays):.3f} minutes\n")
-        else:
-            f.write("- No customers completed Queue 2\n")
-        f.write(f"- Average queue length: {avg_q2:.3f}\n")
-        f.write(f"- Server utilization: {util2:.3f}\n\n")
+        # Request server1 (this handles both queueing and service)
+        with self.server1.request() as req:
+            # This is where the customer would wait in queue1 if servers are busy
+            yield req
+            
+            # Customer got server1 - record delay
+            queue1_delay = self.env.now - queue1_entry
+            self.queue1_delays.append(queue1_delay)
+            del self.queue1_entry_times[customer_id]
+            
+            # Process at server1 (equivalent to EVENT_DEPARTURE1)
+            yield self.env.timeout(self.get_service_time())
         
-        # Simulation Summary
-        f.write(f"Simulation Summary:\n")
-        f.write(f"- Customers completed: {len(completed)}\n")
-        f.write(f"- Simulation duration: {sim_time} minutes\n")
+        # Level 2 processing (equivalent to original depart1 and depart2 logic)
+        
+        # Record queue2 entry time (transfer[4] in original)
+        queue2_entry = self.env.now
+        self.queue2_entry_times[customer_id] = queue2_entry
+        
+        # Request server2
+        with self.server2.request() as req:
+            # Wait in queue2 if servers are busy
+            yield req
+            
+            # Customer got server2 - record delay
+            queue2_delay = self.env.now - queue2_entry
+            self.queue2_delays.append(queue2_delay)
+            del self.queue2_entry_times[customer_id]
+            
+            # Process at server2 (equivalent to EVENT_DEPARTURE2)
+            yield self.env.timeout(self.get_service_time())
+        
+        # Customer completed both services
+        system_time = self.env.now - arrival_time
+        self.system_times.append(system_time)
+        self.num_completed += 1
+    
+    def monitor_queues(self):
+        """Tracks queue lengths over time (equivalent to original filest)"""
+        while True:
+            # Current customers waiting in queue1 (not being served)
+            q1_len = len(self.server1.queue)
+            self.queue1_lengths.append(q1_len)
+            
+            # Current customers waiting in queue2 (not being served)
+            q2_len = len(self.server2.queue)
+            self.queue2_lengths.append(q2_len)
+            
+            yield self.env.timeout(0.1)  # Sample every 0.1 time units
+    
+    def monitor_servers(self):
+        """Tracks server utilization over time"""
+        while True:
+            # Record number of busy servers (equivalent to list_size[LIST_SERVER1])
+            self.server1_busy.append(len(self.server1.users))
+            self.server2_busy.append(len(self.server2.users))
+            yield self.env.timeout(0.1)  # Sample every 0.1 time units
+    
+    def generate_report(self):
+        """Produces output exactly matching the original report format"""
+        def safe_mean(data):
+            return statistics.mean(data) if data else 0.0
+        
+        def safe_max(data):
+            return max(data) if data else 0.0
+        
+        def safe_min(data):
+            return min(data) if data else 0.0
+        
+        report = "Multi-server queueing system using SimPy\n\n"
+        report += f"Mean interarrival time{11*' '}{self.mean_interarrival:.3f} minutes\n\n"
+        report += f"Mean service time{16*' '}{self.mean_service:.3f} minutes\n\n"
+        report += f"Total simulation time{12*' '}{self.env.now:.3f} minutes\n\n"
+        report += f"Distribution type: {self.distribution_type}\n"
+        
+        # Distribution description matching original
+        dist_map = {
+            1: "Interarrival: Exponential, Service: Exponential",
+            2: "Interarrival: Constant, Service: Exponential",
+            3: "Interarrival: Exponential, Service: Constant",
+            4: "Interarrival: Constant, Service: Constant"
+        }
+        report += dist_map.get(self.distribution_type, "Invalid distribution type") + "\n"
+        report += f"Number of servers at level 1: {self.server1.capacity}\n"
+        report += f"Number of servers at level 2: {self.server2.capacity}\n\n"
+        
+        # Queue 1 delays
+        report += "\nDelays in Queue 1, in minutes:\n"
+        report += f"  Average: {safe_mean(self.queue1_delays):.3f}\n"
+        report += f"  Maximum: {safe_max(self.queue1_delays):.3f}\n"
+        report += f"  Minimum: {safe_min(self.queue1_delays):.3f}\n"
+        report += f"  Number measured: {len(self.queue1_delays)}\n"
+        
+        # Queue 2 delays
+        report += "\nDelays in Queue 2, in minutes:\n"
+        report += f"  Average: {safe_mean(self.queue2_delays):.3f}\n"
+        report += f"  Maximum: {safe_max(self.queue2_delays):.3f}\n"
+        report += f"  Minimum: {safe_min(self.queue2_delays):.3f}\n"
+        report += f"  Number measured: {len(self.queue2_delays)}\n"
+        
+        # System times
+        report += "\nTotal System Time (arrival to final departure), in minutes:\n"
+        report += f"  Average: {safe_mean(self.system_times):.3f}\n"
+        report += f"  Maximum: {safe_max(self.system_times):.3f}\n"
+        report += f"  Minimum: {safe_min(self.system_times):.3f}\n"
+        report += f"  Number measured: {len(self.system_times)}\n"
+        
+        # Queue lengths (average)
+        report += "\nQueue lengths:\n"
+        report += f"  Average queue 1 length: {safe_mean(self.queue1_lengths):.3f}\n"
+        report += f"  Average queue 2 length: {safe_mean(self.queue2_lengths):.3f}\n"
+        
+        # Server utilization (percentage)
+        server1_util = sum(self.server1_busy) / len(self.server1_busy) / self.server1.capacity * 100
+        server2_util = sum(self.server2_busy) / len(self.server2_busy) / self.server2.capacity * 100
+        report += "\nServer utilization:\n"
+        report += f"  Server 1 utilization: {server1_util:.1f}%\n"
+        report += f"  Server 2 utilization: {server2_util:.1f}%\n"
+        
+        report += f"\nTime simulation ended:{12*' '}{self.env.now:.3f} minutes\n"
+        report += f"Number of customers completed both services:{8*' '}{self.num_completed}\n"
+        
+        return report
 
-if __name__ == '__main__':
-    main()
+def read_parameters(filename):
+    """Reads parameters from file in the same format as the original"""
+    with open(filename, 'r') as f:
+        # Read all values from the first line
+        values = list(map(float, f.readline().strip().split()))
+        
+        # Convert to appropriate types
+        mean_interarrival = values[0]
+        mean_service = values[1]
+        total_simulation_time = int(values[2])
+        distribution_type = int(values[3])
+        servers_level1 = int(values[4])
+        servers_level2 = int(values[5])
+        
+        return (mean_interarrival, mean_service, total_simulation_time,
+                distribution_type, servers_level1, servers_level2)
+
+def run_simulation(input_file, output_prefix):
+    """Main function to run the simulation"""
+    try:
+        # Read parameters from file
+        params = read_parameters(input_file)
+        mean_interarrival, mean_service, total_simulation_time, \
+        distribution_type, servers_level1, servers_level2 = params
+        
+        # Run simulation
+        env = simpy.Environment()
+        system = TwoLevelQueueSystem(env, mean_interarrival, mean_service,
+                                   servers_level1, servers_level2, distribution_type)
+        env.run(until=total_simulation_time)
+        
+        # Generate and save report
+        report = system.generate_report()
+        output_filename = f"{output_prefix}_{distribution_type}.txt"
+        
+        with open(output_filename, 'w') as f:
+            f.write(report)
+        
+        print(f"Simulation complete. Report saved to {output_filename}")
+        return True
+    
+    except FileNotFoundError:
+        print(f"Error: Cannot open input file {input_file}")
+        return False
+    except Exception as e:
+        print(f"Error running simulation: {str(e)}")
+        return False
+
+if __name__ == "__main__":
+    # Run with the same file names as original
+    input_file = "parameters_9.txt"
+    output_prefix = "report_9"
+    
+    if not run_simulation(input_file, output_prefix):
+        # If parameters_9.txt not found, create a sample one and try again
+        print("Creating sample parameters file and running simulation...")
+        with open(input_file, 'w') as f:
+            f.write("5.0 4.0 1000 1 2 3\n")  # Default parameters
+        
+        run_simulation(input_file, output_prefix)
